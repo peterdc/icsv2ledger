c03ea1ade29c113ca9f00456b3e57e1ca7b1c0c6
Peter Cowan <peterdc@yahoo.com>
2018-01-28 19:59:39 -0800

    remove the file chooser and upgrade to pyinsane2

--- scan.py
+++ scan.py
@@ -1,13 +1,8 @@
 import os
 from PIL import Image
 import icsv2ledger
-# import readline
-# import rlcompleter
-import Tkinter, tkFileDialog
-import pyinsane.abstract as pyinsane
-from pyinsane.abstract import SaneException
+import pyinsane2
 
-canon_driver_options = {'resolution': 300, 'deskew detection': 1, 'auto scan': 1, 'Size': 'Auto Size'}
 floss_driver_options = {'resolution': 300, 'rollerdeskew': 1, 'swdeskew': 1, 'mode': 'Gray', 'swcrop': 1, 'source': 'ADF Duplex', 'page-height': 66182092, 'br-y': 66182092}
 onesided_no_swdeskew = {'resolution': 300, 'rollerdeskew': 1, 'mode': 'Gray', 'swcrop': 1, 'source': 'ADF Front', 'page-height': 66182092, 'br-y': 66182092}
 
@@ -17,12 +12,13 @@ def set_device_options(options, device):
     return device
 
 def setup_scanner(options):
-    devices = pyinsane.get_devices()
-    if len(devices) <= 0:
-        print('no scanner available')
-        return None
-    device = devices[0]
-    print("I'm going to use the following scanner: %s" % (str(device)))
+    pyinsane2.init()
+    try:
+        device = pyinsane2.Scanner(name="canon_dr:libusb:020:008")
+        print("I'm going to use the following scanner: %s" % (str(device)))
+    except PyinsaneException:
+            print("No scanner found")
+            return
     device = set_device_options(options, device)
     return device
 
@@ -49,7 +45,7 @@ def scan_receipt(entry, payee, device, directory):
         if ch in amount:
             amount = amount.replace(ch, '')
 
-    value = icsv2ledger.prompt_for_value('(S)can, (M)ult (C)hoose, or (P)ass', ['Scan', 'Mult', 'Choose', 'Pass'], 'Pass')
+    value = icsv2ledger.prompt_for_value('(S)can, (M)ult or (P)ass', ['Scan', 'Mult', 'Pass'], 'Pass')
     file_extension = ".jpg"
     file_is_pdf = False
     if value:
@@ -57,23 +53,12 @@ def scan_receipt(entry, payee, device, directory):
     else:
         value = 'PASS'
 
-    if value == 'CHOOSE' or value == 'C':
-        root = Tkinter.Tk()
-        root.withdraw()
-
-        orig_image_path = tkFileDialog.askopenfilename()
-        file_name, file_extension = os.path.splitext(orig_image_path)
-        if file_extension.upper() == ".PDF":
-            file_is_pdf = True
-            img = orig_image_path
-        else:
-            img = Image.open(orig_image_path)
     elif value == 'SCAN' or value == 'S':
         scan_session = device.scan(multiple=False)
         try:
             while True:
                 scan_session.scan.read()
-        except SaneException:
+        except PyinsaneException:
             scan_session.scan.cancel()
             raise
         except EOFError:
@@ -87,7 +72,7 @@ def scan_receipt(entry, payee, device, directory):
             try:
                 while True:
                     scan_session.scan.read()
-            except SaneException:
+            except PyinsaneException:
                 scan_session.scan.cancel()
                 raise
             except EOFError:
@@ -105,37 +90,9 @@ def scan_receipt(entry, payee, device, directory):
     elif value == 'PASS' or value == 'P':
         return
     else:
-        raise ValueError('Must be one of: Scan, Mult, Choose, or Pass')
+        raise ValueError('Must be one of: Scan, Mult, or Pass')
 
     output_file_name = '{0}_{1}_{2}{3}'.format(entry.date, payee, amount, file_extension)
     output_file_path = os.path.join(directory, output_file_name)
-    if file_is_pdf:
-        os.rename(orig_image_path, output_file_path)
-    else:
-        if img.mode != 'RGB':
-            img = img.convert('RGB')
-        img.save(output_file_path, "JPEG")
     return(output_file_path)
 
-# def prompter(prompt, values, default):
-#
-#     def completer(text, state):
-#         for val in values:
-#             if text.upper() in val.upper():
-#                 if not state:
-#                     return val
-#                 else:
-#                     state -= 1
-#         return None
-#
-#     # There are no word deliminators as each account name
-#     # is one word.  eg ':' and ' ' are valid parts of account
-#     # name and don't indicate a new word
-#     readline.set_completer_delims("")
-#     readline.set_completer(completer)
-#     if 'libedit' in readline.__doc__:
-#         readline.parse_and_bind("bind ^I rl_complete")
-#     else:
-#         readline.parse_and_bind("tab: complete")
-#
-#     return raw_input('{0} [{1}] > '.format(prompt, default))
